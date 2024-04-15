2e025153cef3d4932c086e3d90e9e99ae4a850e3
EzryStIago <ezrysm@gmail.com>
2024-04-14 21:47:47 -0400

    add documentation

--- Notebook_Helpers.py
+++ Notebook_Helpers.py
@@ -13,6 +13,21 @@ import re
 
 
 def get_num_regex(regex, fname, grp=2):
+    """
+    Extracts a number from a file using a regular expression.
+
+    Args:
+        regex (str): The regular expression pattern to search for.
+        fname (str): The name of the file to search in.
+        grp (int, optional): The group number to extract from the regex match. Defaults to 2.
+
+    Returns:
+        str: The matched number as a string.
+
+    Example:
+        >>> get_num_regex('\d+', 'data.txt')
+        '123'
+    """
     with open(fname) as fin:
         fstring = fin.read()
         return re.search(regex, fstring).group(grp)
@@ -22,13 +37,86 @@ uw_regex = '(upperWalls[\ \t]+)(\d+.\d+)'
 get_upper_walls = lambda fname: get_num_regex(uw_regex, fname)
 
 def P_bind(K, L):
-    return L/(K+L)
+    """
+    Calculate the fraction of binding sites occupied given the dissociation constant and ligand concentration.
+
+    Args:
+        K (float): The dissociation constant of the ligand.
+        L (float or array-like): The concentration of the ligand.
+
+    Returns:
+        float or array-like: The fraction of binding sites occupied, calculated using the formula L / (K + L).
+    """
+    return L / (K + L)
 
 def Kd(dG, RT):
+    """
+    Calculate the dissociation constant (Kd) from the Gibbs free energy change (dG) and the product of the gas constant and temperature (RT).
+
+    Args:
+        dG (float): The Gibbs free energy change in kcal/mol.
+        RT (float): The product of the gas constant (R) and temperature (T) in kcal/mol.
+
+    Returns:
+        float: The dissociation constant (Kd) in microMolar (µM).
+    """
     return np.exp(dG/RT)*1000000
+
+
+def plot_titration(ax, concentrations, dG_binding, error_binding, RT):
+    """
+    Plot the titration curve for a given set of concentrations and binding energies, including a 95% confidence interval.
+
+    Args:
+        ax (matplotlib.axes.Axes): The matplotlib axis object where the plot will be drawn.
+        concentrations (array-like): Array of ligand concentrations in microMolar.
+        dG_binding (float): The Gibbs free energy change in kcal/mol for binding.
+        error_binding (float): The standard error of the Gibbs free energy change.
+        RT (float): The product of the gas constant (R) and temperature (T) in kcal/mol.
+
+    Returns:
+        matplotlib.axes.Axes: The axis object with the titration plot.
+
+    """
+    K = Kd(dG_binding, RT)
+
+    ax.plot(concentrations, P_bind(K, concentrations), label='Binding Curve')
+
+    P_lower = P_bind(Kd(dG_binding-error_binding*1.96, RT),concentrations)
+    P_upper = P_bind(Kd(dG_binding+error_binding*1.96, RT), concentrations)
+    ax.fill_between(concentrations, P_lower, P_upper, alpha=0.25, label='95% Confidence Interval')
+    plt.xscale('log')
+    ax.set_xlabel('Concentration of Phenol '+r'($\mathrm{\mu}$M)', fontsize=20)
+    ax.set_ylabel('Fraction of Sites Occupied', fontsize=20)
+    ax.set_xticklabels(ax.get_xticklabels(), fontsize=16)
+    ax.set_yticklabels(ax.get_yticklabels(), fontsize=16)
+    ax.vlines(K, 0, 1, linestyles='dashed', color='black', label='Dissociation Constant')
+    ax.legend(loc='lower right', fontsize=20*0.75)
+
+    return ax
+
 
 @dataclass
 class dGData:
+    """
+    A data class representing dG data for free energy calculations.
+
+    Attributes:
+        inpath (Path): The path to the data files.
+        filepattern (str): The pattern to match the data file names.
+        temperature (float): The temperature in Kelvin.
+        name (str): The name of the dG data.
+        detectEQ (bool, optional): Whether to detect equilibrium in the data. Defaults to True.
+        perWindow (pd.DataFrame, optional): The per-window data. Defaults to an empty DataFrame.
+        cumulative (pd.DataFrame, optional): The cumulative data. Defaults to an empty DataFrame.
+        dG (float, optional): The calculated dG value. Defaults to 0.
+        error (float, optional): The estimated error in the dG value. Defaults to 0.
+        RT (float, optional): The product of the gas constant and temperature. Defaults to 0.
+
+    Methods:
+        pretty_print_dG(): Returns a formatted string representation of the dG and error values.
+
+    """
     inpath: Path
     filepattern: str
     temperature: float
@@ -41,6 +129,12 @@ class dGData:
     RT: float = 0
 
     def pretty_print_dG(self):
+        """
+        Generates a formatted string representing the Gibbs free energy change (ΔG) and its error, using HTML markup for styling.
+
+        Returns:
+            str: A string formatted with HTML to display the Gibbs free energy change and its error. The ΔG value is subscripted with the name of the data, and both ΔG and error values are presented in kcal/mol. The string is formatted to display in size 5 font.
+        """
         #\u0394 == Delta
         change_mkd_site = f'\u0394G<sub>{self.name}</sub> = {self.dG} kcal/mol'
         error_mkd_site = f'PyMBAR estimated error: {self.error} kcal/mol'
